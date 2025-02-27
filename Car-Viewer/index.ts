427368568ac3054f8ae40a73237efa10d7d30f94
malik0809 <159032371+malik0809@users.noreply.github.com>
2025-02-27 21:15:46 +0000

    aanpassingen json bestanden

--- Car-Viewer/index.ts
+++ Car-Viewer/index.ts
@@ -1,2 +1,15 @@
 import * as readline from "readline";
+import { Car } from "./interfaces";
+import { Dealership } from "./interfaces";
+
+(async function () {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/malik0809/project/refs/heads/main/bmw-cars.json');
+        const posts: Car[] = await response.json();
+        console.log(posts[0].name);
+    } catch (error: any) {
+        console.log(error);
+    }
+})();
+
 
